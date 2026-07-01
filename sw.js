be239292793c5efe844d10d47138cdbacbace713
Oderli Sergio Garcia <161622446+garcia-oderli@users.noreply.github.com>
2026-07-01 20:06:11 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gastosIA-v4.1';
+const CACHE = 'gastosIA-v4.2';
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', '/index.html', '/manifest.json', '/icon.svg'])));
   self.skipWaiting();
